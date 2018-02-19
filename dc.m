571e122a967591945c9a25177a733055904cc24b
Namrata7199 <namrataladda@gmail.com>
2018-02-19 18:33:40 +0530

    This is a rough code. There is some small error.

---
+++ dc.m
@@ -0,0 +1,47 @@
+n=input('length');
+for i=1:n
+    for j=1:2
+        a(i,j)=input('prompt');
+    end
+end
+for i=1:n
+    code(i)=0;
+end
+for i=1:8
+    is(i)=0;
+end
+for i=1:9
+    im(i)=0;
+end
+dist=0;
+for i=1:n
+    disp(is);
+    fprintf("\n");
+    for j=1:7
+        is(j+1)=is(j);
+    end
+    is(1)=0;
+    im(9)=im(8);
+    disp(is);
+    for j=1:8
+        im(j)=is(j);
+    end
+    disp(im);
+    out1=[im(1)^im(2)^im(4)^im(5)^im(6) im(1)^im(2)^im(3)^im(5)^im(6)];
+    is(1)=1;
+    im(1)=1;
+    out2=[im(1)^im(2)^im(4)^im(5)^im(6) im(1)^im(2)^im(3)^im(5)^im(6)];
+    lk=(a(i,1)^out1(1))+(a(i,2)^out1(2));
+    m=(a(i,1)^out2(1))+(a(i,2)^out2(2));
+    if(lk<m)
+        im(1)=0;
+        is(1)=0;
+        dist=dist+lk;
+        code(i)=0;
+    else
+        dist=dist+m;
+        code(i)=1;
+    end
+    
+end
+disp(code);
